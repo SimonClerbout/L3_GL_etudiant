8a0b49afa280166b2ece9a6950eb73a39ed56dba
SimonClerbout <simon.clerbout@laposte.net>
2018-02-22 11:34:29 +0100

    cmake

---
+++ TP_compilation/imshow/build/CMakeFiles/imshow.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imshow.out.dir/imshow.cpp.o"
+  "imshow.out.pdb"
+  "imshow.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imshow.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
