18ee7f1f53e3d77437518d12b63caebc1fba8ced
SimonClerbout <simon.clerbout@laposte.net>
2018-02-22 11:54:16 +0100

    play_video

---
+++ TP_compilation/play_video/build/CMakeFiles/play_video_gui.out.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/play_video_gui.out.dir/src/play_video_gui/play_video_gui.cpp.o"
+  "CMakeFiles/play_video_gui.out.dir/src/play_video_gui/PlayerWindow.cpp.o"
+  "CMakeFiles/play_video_gui.out.dir/src/play_video/Filesystem.cpp.o"
+  "play_video_gui.out.pdb"
+  "play_video_gui.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/play_video_gui.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
